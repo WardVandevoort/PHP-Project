b0b3f75d6046bc1a3bda1aca102398e88c0d101a
Ward Vandevoort <vandevoort.ward@gmail.com>
2020-03-23 12:40:48 +0100

    settings.php file toegevoegd, zodat database connectie makkelijk kan aangepast worden

--- classes/database.php
+++ classes/database.php
@@ -6,10 +6,10 @@ private static $conn;
 
 public static function getConnection(){
 
-//include_once(__DIR__ . "/../settings/settings.php");
+include_once(__DIR__ . "/../settings/settings.php");
 
     if(self::$conn === null){
-        self::$conn = new PDO("mysql:host=localhost; dbname=php-project", "root", "" );
+        self::$conn = new PDO("mysql:host=" . SETTINGS["db"]["host"] . "; dbname=" . SETTINGS["db"]["dbname"], SETTINGS["db"]["user"], SETTINGS["db"]["password"] );
         
         return self::$conn;
     }
