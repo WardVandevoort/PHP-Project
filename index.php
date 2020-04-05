a19526dd478258ffb707821f5377439b48303cd9
COROJ <jorre1996@hotmail.com>
2020-04-05 13:38:09 +0200

    feature 6 done

--- index.php
+++ index.php
@@ -14,11 +14,23 @@ $_SESSION["id"] = $data["id"];
 
 if($data["new"] == true){
    header("Location: profielVervolledigen.php");
+};
+
+
+include_once(__DIR__ . "/classes/search.php");
+
+if (isset($_POST['search'])){
+
+    $dataSearch = null;
+
+    try {
+        $search = new Search();
+        $search -> setSearch($_POST['search']);
+        $dataSearch = $search -> fetchData();
+    } catch (\Throwable $th) {
+        $error = $th -> getMessage();
+    }
 }
-
-
-
-
 
 
 
@@ -40,10 +52,22 @@ if($data["new"] == true){
 <?php include_once("inc.nav.php"); ?>
 
 <h1>Dit is de homepage</h1>
-<form action="search.php">
+<form action="index.php" method="POST">
     <input type="text" name="search" placeholder="Zoeken...">
     <button type="submit" name="submit-search">Zoeken</button>
 </form>
+
+<?php 
+
+foreach($dataSearch as $result){ ?>
+    <div>
+                <img src="avatars/<?php echo $result["avatar"] ?>" alt="avatar">
+                <h3><?php echo $result['firstname'] . " " . $result['lastname']?></h3>
+                <h4><?php echo $result['year']?></h4>
+                
+    </div>
+    <?php } ?>
+
 
 </body>
 </html>
