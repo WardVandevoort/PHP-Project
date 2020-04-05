c1fa8f52f16f9d7db46f8c4614fe99665a06ed73
COROJ <jorre1996@hotmail.com>
2020-04-05 14:51:25 +0200

    fixed warnings and notice messages

--- index.php
+++ index.php
@@ -19,17 +19,20 @@ if($data["new"] == true){
 
 include_once(__DIR__ . "/classes/search.php");
 
+$dataSearch = null;
+
 if (isset($_POST['search'])){
 
-    $dataSearch = null;
-
-    try {
-        $search = new Search();
-        $search -> setSearch($_POST['search']);
-        $dataSearch = $search -> fetchData();
-    } catch (\Throwable $th) {
-        $error = $th -> getMessage();
-    }
+   
+   
+        try {
+            $search = new Search();
+            $search -> setSearch($_POST['search']);
+            $dataSearch = $search -> fetchData();
+        } catch (\Throwable $th) {
+            $error = $th -> getMessage();
+        }
+    
 }
 
 
@@ -59,15 +62,16 @@ if (isset($_POST['search'])){
 
 <?php 
 
-foreach($dataSearch as $result){ ?>
-    <div>
-                <img src="avatars/<?php echo $result["avatar"] ?>" alt="avatar">
-                <h3><?php echo $result['firstname'] . " " . $result['lastname']?></h3>
-                <h4><?php echo $result['year']?></h4>
-                
-    </div>
-    <?php } ?>
-
+if( $dataSearch != null) {
+    foreach($dataSearch as $result){ ?>
+        <div>
+                    <img src="avatars/<?php echo $result["avatar"] ?>" alt="avatar">
+                    <h3><?php echo htmlspecialchars($result['firstname'] . " " . $result['lastname'])?></h3>
+                    <h4><?php echo htmlspecialchars($result['year']) ?></h4>
+                    
+        </div>
+        <?php }
+} ?>
 
 </body>
 </html>
