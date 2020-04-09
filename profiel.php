b0b8e85e43295d333da781840251be434d4287eb
Ward Vandevoort <vandevoort.ward@gmail.com>
2020-04-09 14:56:00 +0200

    Changed displayed value of buddy, was the same value as sport

--- profiel.php
+++ profiel.php
@@ -242,10 +242,11 @@ if(!empty($_POST)){
             </div>
 
             <div>
-                <p>Ik ben hier: <?php echo $data["sport"] ?></p>
+                <p>Ik ben hier: <?php echo $data["buddy"] ?></p>
                 <label for="buddy" class="showInput changeLabel">verander je doel</label>
                 <select class="inputField showInput" name="buddy" id="buddy">
-                    <option value="buddy" selected="selected">om een buddy te worden</option>
+                <option id="select9" value="" selected="selected" disabled>Doel</option>
+                    <option value="buddy">om een buddy te worden</option>
                     <option value="begleider1">om een begleider van 1IMD te zijn </option>
                     <option value="begleider2">om een begleider van 2IMD te zijn</option>
                 </select>
